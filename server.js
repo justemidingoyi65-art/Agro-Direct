6bba9fe853eae626191809cbd9a5f292240ade4b
justemidingoyi65-art <justemidingoyi65@gmail.com>
2026-02-27 22:08:50 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,4 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const productRoutes = require('./routes/p'); // On utilise le nom 'p' que tu as créé
-
 const app = express();
-app.use(cors());
-app.use(express.json());
-
-// Connexion à la base de données
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('Connecté à MongoDB'))
-  .catch(err => console.log('Erreur MongoDB:', err));
-
-// Routes
-app.use('/api/products', productRoutes);
-
-// Route de test pour voir si ça marche
-app.get('/', (req, res) => res.send('Le serveur fonctionne !'));
-
-const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => console.log('Serveur lancé sur le port ' + PORT));
-
-
-
-
+app.get('/', (req, res) => res.send('TEST REUSSI'));
+app.listen(process.env.PORT || 10000, () => console.log('OK'));
